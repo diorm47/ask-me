a25d1f86a50baef41a865e0e22a3e537cb2a15c4
diorm47 <dior47m@gmail.com>
2024-09-24 16:22:50 +0500

    mobile & adaptive

--- scripts/profile-tabs.js
+++ scripts/profile-tabs.js
@@ -23,3 +23,31 @@ if (tabs) {
     }
   };
 }
+
+// mob
+
+const mtabs = document.querySelector(".tabs_list_m");
+const mtabButton = document.querySelectorAll(".menu_item_m");
+const mcontents = document.querySelectorAll(".menu_item_content_m");
+
+if (mtabs) {
+  mtabs.onclick = (e) => {
+    let target = e.target;
+    while (target && !target.classList.contains("menu_item_m")) {
+      target = target.parentNode;
+    }
+    if (target && target.dataset.id) {
+      const id = target.dataset.id;
+      mtabButton.forEach((btn) => {
+        btn.classList.remove("active_menu");
+      });
+      target.classList.add("active_menu");
+
+      mcontents.forEach((content) => {
+        content.classList.remove("active_menu");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active_menu");
+    }
+  };
+}
